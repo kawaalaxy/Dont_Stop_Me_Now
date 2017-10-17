5b81dcb19e1304456c6bbb747c444e13f911670d
kawaalaxy <32813468+kawaalaxy@users.noreply.github.com>
2017-10-17 19:02:22 +0200

    Add files via upload

---
+++ HigherOrder.ml
@@ -0,0 +1,36 @@
+(************************************************************)
+(*                 Higher Order Functions                   *)
+(************************************************************)
+
+(*****************     Toolbox   *******************)
+(*                 Char and String                 *)
+let char_type c =
+    if int_of_char c < 123 && int_of_char c > 96 then
+      "lower"
+    else
+      if int_of_char c < 91 && int_of_char c > 64 then
+	"upper"
+      else
+	"other";;
+
+
+
+(*                   Higher Order                  *)
+
+
+(*****************     Cipher    *******************)
+(*                  Ceasar Cipher                  *)
+
+(*                 Vigenere Cipher                 *)
+
+(*****************  Build House  *******************)
+
+let sand = ['B' ; 'b' ; 'L' ; 'r' ; 'B'; 'B' ; '\n'];;
+let water = ['y' ; 'o' ; 'y' ; 'y' ; 'i'; 'y'; '\n'];;
+let brick = ['y' ; 's' ; 's' ; 's' ; 's'; 'e'; '\n'];;
+let wood = ['h' ; 'r' ; 'w' ; 'w' ; 'r'; 'h'; '\n'];;
+let coca = ['s' ; 'l' ; 'r' ; 'x' ; 'l'; 's'; '\n'];;
+
+let house = [sand ; water ; brick ; wood ; coca];;
+
+let workers = [lowercase ; swap_alpha ; rot13 ; rotn 5 ; rotn 20];;
